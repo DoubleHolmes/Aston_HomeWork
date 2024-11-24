b6884f09b056c39441bc6a8a5f3389c0f55baed4
DoubleHolmes <lipko.ln@yandex.by>
2024-11-24 18:26:10 +0300

    Выполнено седьмое задание. Создан метод returnResult(). Была добавлена возможность ввода данных из класса main. В 6 задании выполнена передача переменной в метод.

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -3,6 +3,7 @@ package org.example;
 import org.example.lessonFour.exerciseFive.ExerciseFive;
 import org.example.lessonFour.exerciseFour.ExerciseFour;
 import org.example.lessonFour.exerciseOne.ExerciseOne;
+import org.example.lessonFour.exerciseSeven.ExerciseSeven;
 import org.example.lessonFour.exerciseSix.ExerciseSix;
 import org.example.lessonFour.exerciseThree.ExerciseThree;
 import org.example.lessonFour.exerciseTwo.ExerciseTwo;
@@ -13,43 +14,32 @@ import java.util.Scanner;
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
         // Задание 1.
-        new ExerciseOne().printThreeWords();
-        // Задание 2.
-        new ExerciseTwo().checkSumSign();
-        // Задание 3.
-        new ExerciseThree().printColor();
-        // Задание 4.
-        new ExerciseFour().compareNumbers();
-        //Задание 5.
-        new ExerciseFive().checkNumbers();
+//        new ExerciseOne().printThreeWords();
+//        // Задание 2.
+//        new ExerciseTwo().checkSumSign();
+//        // Задание 3.
+//        new ExerciseThree().printColor();
+//        // Задание 4.
+//        new ExerciseFour().compareNumbers();
+//        //Задание 5.
+//        new ExerciseFive().checkNumbers();
         // Задание 6.
-        new ExerciseSix().positiveOrNegative();
+        System.out.println("Задание 6.\n Введите целое число: ");
+        int a = scanner.nextInt();
+        new ExerciseSix().positiveOrNegative(a);
         // Задание 7.
-        //returnResult();
+        System.out.println("Задание 7.\n  Введите целое число: ");
+        int b = scanner.nextInt();
+        new ExerciseSeven().returnResult(b);
         // Задание 8.
         //printLine("Привет всем!", 5);
         //Задание 9.
         //leapYear();
         //Задание 10.
-        arrayChange();
+       // arrayChange();
 
-    }
-
-
-
-    // Задание 6.
-
-
-    // Задание 7.
-    public static void returnResult() {
-        Scanner scanner = new Scanner(System.in);
-        System.out.println("Задание 7.");
-        System.out.print("Введите целое число: ");
-        int a = scanner.nextInt();
-        boolean results = (a < 0);
-        System.out.println("Ответ: " + results);
-        scanner.close();
     }
     // Задание 8.
         public static void printLine(String text, int count) { //метод принимает параметры текст и кол-во
--- src/main/java/org/example/lessonFour/exerciseSix/ExerciseSix.java
+++ src/main/java/org/example/lessonFour/exerciseSix/ExerciseSix.java
@@ -3,17 +3,12 @@ package org.example.lessonFour.exerciseSix;
 import java.util.Scanner;
 /*Метод, проверящий, явяляется ли число введеное с консоли, положительным или отрицательным. */
 public class ExerciseSix {
-    public void positiveOrNegative() {
-        Scanner scanner = new Scanner(System.in);
-        System.out.println("Задание 6.");
-        System.out.print("Введите целое число: ");
-        double a = scanner.nextDouble();
+    public void positiveOrNegative(int a) {
         if (a >= 0) {
             System.out.println("Ответ: Число положительно");
         }
         else {
             System.out.println("Ответ: Число отрицательное");
         }
-        scanner.close();
     }
 }
